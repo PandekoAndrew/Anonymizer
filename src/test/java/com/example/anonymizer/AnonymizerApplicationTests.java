621c21d110ed5049fd558781c00f527767aa7c8f
PandekoAndrew <pandeko.andrei@gmail.com>
2022-12-29 13:50:38 +0500

    initial commit

---
+++ src/test/java/com/example/anonymizer/AnonymizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anonymizer;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
